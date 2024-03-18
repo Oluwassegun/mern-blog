c25b43a05ad7d9e1dd345ba8fbe8d9e41247dd2e
Oluwasegun <oluwasegunenoch_adedeji@yahoo.com>
2024-03-18 06:51:42 +0000

    create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -31,7 +31,7 @@ export default function Header() {
             <FaMoon />
         </Button>
         <Link>
-        <Button gradientDuoTone='purpleToBlue'>
+        <Button gradientDuoTone='purpleToBlue' outline>
             Sign In
         </Button>
         </Link>
